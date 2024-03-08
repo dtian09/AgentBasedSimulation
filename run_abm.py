0a1b5bc3a59169868d319f6950c9451181413b27
David Tian <d.tian@sheffield.ac.uk>
2024-03-08 18:08:34 +0000

    fixed-bugs

---
+++ run_abm.py
@@ -0,0 +1,24 @@
+# ABM software for smoking behaviour using the COM-B theories: regular smoking uptake theory, quit attempt theory and
+# quit success theory and relapse theory
+#
+# Running command: python main.py props/model.yaml
+
+from __future__ import annotations
+from mpi4py import MPI
+from repast4py import parameters
+
+from config.definitions import ROOT_DIR
+from smokingcessation.smoking_model import SmokingModel
+
+
+def main():
+    parser = parameters.create_args_parser()
+    args = parser.parse_args()
+    params = parameters.init_params(f'{ROOT_DIR}/' + args.parameters_file, args.parameters)
+    model = SmokingModel(MPI.COMM_WORLD, params)
+    model.init()
+    model.run()
+
+
+if __name__ == "__main__":
+    main()
